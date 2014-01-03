3a173ce9505b08abda344ceeb3bb7cbc898bc55c
sokolmkd <sokolpwns@gmail.com>
2014-01-03 02:23:21 +0100

    Viktor dzver43
    Initiral

---
+++ FinancialCalculator/gen/com/team_argati/financialcalculator/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.team_argati.financialcalculator;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
