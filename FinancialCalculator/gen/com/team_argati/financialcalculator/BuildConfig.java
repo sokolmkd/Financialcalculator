41b44d60bf1072794d88dff37497b559b0ab613e
Flixter <Vic_petrovski@hotmail.com>
2014-01-03 02:49:45 +0100

    Izbrishani
    Izbrishano ?

--- FinancialCalculator/gen/com/team_argati/financialcalculator/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.team_argati.financialcalculator;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
